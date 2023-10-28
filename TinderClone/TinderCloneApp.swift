a845da101efd6c2f7697d22cff853a03a5aec318
eddie-eights <fuk8eightuda@gmail.com>
2023-10-28 11:01:13 +0900

    Initial Commit

---
+++ TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by 福田瑛斗 on 2023/10/28.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
